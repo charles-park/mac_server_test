c192b77417e3d9946074bf4c88b6b0c9c6c52341
charles.park <charles.park@hardkernel.com>
2022-04-19 09:36:42 +0000

    error msg fix

--- mac_server_ctl.py
+++ mac_server_ctl.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     board_name = 'None'
 
     if args_cnt != 3:
-        print ('error:')
         print ('usage : python3 mac_server_ctl.py [option1] [option2]')
         print ('      [option1] r : request mac addr [option2 {board name}]')
         print ('      [option1] e : erase mac addr [option2 {erase mac addr}]')
@@ -19,34 +18,43 @@ if __name__ == "__main__":
         print ('           python3 mac_server_ctl.py e 001e06123456')
         quit()
 
-    if args[1] == 'r':
-        board_name = args[2]
-        print ('request mac address', 'board name =', board_name)
-    elif args[1] == 'e':
-        mac_addr = args[2]
-        print ('erase mac addr', 'mac address =', mac_addr)
-    else :
-        print ("error: unknown message", f'args[1] = {args[1]}, args[2] = {args[2]}')
-        quit()
-    
     # odroid mac server control class get
-    api_manager = API_MANAGER(board=board_name)
-
+    api_manager = API_MANAGER(board=args[2])
     # Log message disaplay control (1 = enable)
     api_manager.log_en(0)
     # api infomation (server info) display
     api_manager.info_print()
-    
-    # get mac address for board-name from the mac server.
-    uuid = asyncio.run(api_manager.request_mac_addr())
-    # info update
-    asyncio.run(api_manager.update_record({'uuid': uuid}))
 
-    # remove current mac address info or set mac address
-    # api_manager.set_mac_addr ('001e06510034')
-    asyncio.run(api_manager.delete_assigned_sign())
+    if args[1] == 'r':
+        # get mac address for board-name from the mac server.
+        uuid = asyncio.run(api_manager.request_mac_addr())
+        if "error" in uuid :
+            print ('error, request_mac_addr()!\n', uuid)
+            quit()
+        else :
+            # info update
+            asyncio.run(api_manager.update_record({'uuid': uuid}))
+    elif args[1] == 'e':
+        # remove current mac address info or set mac address
+
+        if '001e06' in args[2] and len(args[2]) == 12:
+            api_manager.set_mac_addr (args[2])
+            msg = asyncio.run(api_manager.delete_assigned_sign())
+        else:
+            print ('error, input mac addr,', f'input mac = {args[2]}, mac len = {len(args[2])}')
+            quit()
+        if "error" in msg:
+            print ('error, delete_assigned_sign()', msg)
+            quit()
+    else :
+        print ("error, unknown message", f'args[1] = {args[1]}, args[2] = {args[2]}')
+        quit()
 
     # mac display
     if "001e06" in uuid:
         ret_msg = 'sucess' + ',' + api_manager.mac_addr + ',' + api_manager.uuid_mac
         print (ret_msg)
+        api_manager.log_en(1)
+        api_manager.info_print()
+        # if test
+        asyncio.run(api_manager.delete_assigned_sign())
